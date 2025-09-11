f6be016bdd1126493099b8b148fe38b1b8d1aa7c
Tea-September <2921676100@qq.com>
2025-09-11 21:50:24 +0800

    Learn TileMapLayer
    2025/9/11 对TileMapLayer进行深度学习

--- mythirdgame/Scripts/world.gd
+++ mythirdgame/Scripts/world.gd
@@ -4,11 +4,11 @@ extends Node2D
 @onready var camera_2d: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-	# 获得矩形框，以图块为单位
-	var used = tile_map_layer.get_used_rect()
-	# 转换，以像素为单位
+	# 包围所有图块，形成矩形框，包含position和end属性，分别是指左上角和右下角，都为从原点数第几个格子，position为负，end为正
+	var used = tile_map_layer.get_used_rect().grow(-1)
+	# 获取图块的长和宽，x为宽、y为长a，并且转换，以像素为单位
 	var tile_size = tile_map_layer.tile_set.tile_size
-	# camera_2d.limit_top为设置顶部极限坐标，used.position为获取图块左上角坐标
+	# camera_2d.limit_top为设置顶部极限坐标
 	camera_2d.limit_top = used.position.y * tile_size.y
 	camera_2d.limit_right = used.end.x * tile_size.x
 	camera_2d.limit_bottom = used.end.y * tile_size.y
